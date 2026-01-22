9773607e17c792a0fe55b0dc79bf4a7026a1939d
VJsharan <v.vijaysharan@gmail.com>
2026-01-22 17:25:32 +0530

    learnt basic datatypes and objects

---
+++ javascript1.js
@@ -0,0 +1,29 @@
+/* console.log("hello world");
+let name = "vijay"; //variable declaration 
+console.log(name); //printing it
+console.log(typeof(name)); //printing the type of the variable  
+
+shift alt A for multi line comment
+
+
+const pie = 3.14; dealing with a float var 
+console.log(pie*3)
+
+here the primitive data types are string, number, boolean, undefined, null
+
+
+const boolval = true;
+console.log(typeof(boolval));  // dealing with a boolean var        
+
+
+let vijay;
+console.log(vijay);
+vijay = "my name";
+console.log(vijay);
+
+
+
+let x = null;
+console.log(typeof(x)); //when we want to intentionally denote the absence of a value we use null
+
+*/
---
+++ objects.js
@@ -0,0 +1,13 @@
+let data = {
+    name: "vijay",
+    age: 20,
+    gender: 'M',
+    isstudent: true
+};
+
+console.log(data);
+console.log(typeof (data));
+console.log(data.name);        //two ways to print its content
+console.log(data['age']);
+
+// this is how to declare an object and print its content 
